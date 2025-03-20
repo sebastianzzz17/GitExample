3ffb2cc4a596d2322c53a9a0d558351ef591020e
Sebastian Campillo Ordoñez <sebastiancampillo@uninorte.edu.co>
2025-03-20 08:21:55 -0500

    Feat: Added Person class

---
+++ src/core/Person.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author sebastiancampillo
+ */
+public class Person {
+    private int id ;
+    private String name;
+    private int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", name=" + name + ", age=" + age + '}';
+    }
+    
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,8 @@
  */
 package main;
 
+import core.Person;
+
 /**
  *
  * @author Autologon
@@ -11,7 +13,8 @@ package main;
 public class Main {
     
     public static void main(String[] args) {
-        System.out.println("Hello world!!");
+      Person person = new  Person ( 784835854,"Juan perez",23);
+        System.out.println(person);
     }
     
 }
