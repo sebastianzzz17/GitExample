c3662e1021e5910e89f5fe7b59889ee7f21b5b0c
Sebastian Campillo Ordoñez <sebastiancampillo@uninorte.edu.co>
2025-03-20 08:37:04 -0500

    this is class Dog

---
+++ src/core/Dog.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author sebastiancampillo
+ */
+public class Dog {
+    private String name;
+    private String raza;
+    
+}
